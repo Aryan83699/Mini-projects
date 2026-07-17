99d1cf4c101795dfc78ae042e2e9e871a79140b7
Aryan Singh <singharyan90827@gmail.com>
2026-07-17 12:31:17 +0530

    computer player added

---
+++ 8. Tic-Tac-Toe/one-player vs computer/app.py
@@ -0,0 +1,30 @@
+from flask import Flask,request,render_template
+
+app=Flask(__name__)
+
+
+
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('player.html')
+
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+@app.route('/choice' ,methods=['GET','POST'])
+def choice():
+    player_val1=request.form.get('O')
+    player_val2=request.form.get('X')
+    print(player_val1,player_val2)
+    
+    if player_val1:
+        return render_template('game.html',player=player_val1)
+
+    return render_template('game.html', player=player_val2)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ 8. Tic-Tac-Toe/one-player vs computer/static/js/script.js
@@ -0,0 +1,147 @@
+let board=document.querySelector(".board");
+let boxes=board.querySelectorAll('.boxes');
+let resetButton=document.querySelector('#reset');
+let win_container=document.querySelector(".win-container");
+let h2=document.querySelector(".win-container h2");
+let newGame=document.querySelector("#newGame");
+
+
+let turnO;
+let computer;
+let gameOver=false;
+
+let winPatterns=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+];
+
+
+if(player=='X'){
+   turnO=false; 
+   computer='O';
+}
+else{
+    turnO=true;
+    computer='X';
+}
+
+let moves=['X','O'];
+
+
+let humanTurn=true;
+
+
+const disb_all =()=>{
+    for(box of boxes){
+        box.disabled=true;
+    }
+}
+
+
+const empty_boxes = () =>{
+        for(box of boxes){
+        box.innerText="";
+        box.disabled=false;
+    }
+}
+
+
+newGame.addEventListener('click',()=>{
+    empty_boxes();
+    win_container.classList.add('hide');
+})
+
+resetButton.addEventListener('click',()=>{
+    empty_boxes();
+}
+)
+
+
+function checkWinner(){
+    for(patterns of winPatterns){
+        let pos1=boxes[patterns[0]].innerText;
+        let pos2=boxes[patterns[1]].innerText;
+        let pos3=boxes[patterns[2]].innerText;
+
+        if(pos1=='X' && pos2=='X' && pos3=='X'){
+            gameOver=true;
+            h2.innerText="Winner is X"
+            win_container.classList.remove('hide');
+
+            disb_all()
+            
+        }
+        else if(pos1=='O' && pos2=='O' && pos3=='O'){
+            gameOver=true;
+            h2.innerText="Winner is O"
+            win_container.classList.remove('hide');
+            disb_all()
+        }
+    }
+}
+
+
+function play () {
+    boxes.forEach(box => {
+    box.addEventListener('click',()=>{
+        if(turnO){
+            box.innerText=player;
+            turnO=false;
+            
+        }
+        else{
+            box.innerText=player;
+            turnO=true;
+            
+        }
+        box.disabled=true;
+        checkWinner();
+        if (!gameOver) {
+            setTimeout(() => {
+                comp_play(computer);
+            }, 800);
+        }
+    })
+    
+});
+
+}
+
+
+play();
+
+
+
+const enable_all =()=>{
+    for(box of boxes){
+        box.disabled=false;
+    }
+}
+
+
+const comp_play=(player_val)=>{
+    disb_all();
+    let empty=[];
+
+    boxes.forEach((box,index)=>{
+        if(box.innerText==""){
+            empty.push(index);  
+        }
+
+    })
+
+    let randomIndex = Math.floor(Math.random() * empty.length);
+    let move = empty[randomIndex];
+
+    boxes[move].innerText = player_val;
+    boxes[move].disabled = true;
+
+    enable_all();
+    
+}

10254a00e13e6b39e622f9396baca25d82aa45c3
Aryan Singh <singharyan90827@gmail.com>
2026-07-17 13:59:16 +0530

    Login UI created _flask

--- 8. Tic-Tac-Toe/one-player vs computer/app.py
+++ 8. Tic-Tac-Toe/one-player vs computer/app.py
@@ -10,10 +10,10 @@ def home():
 
 
 
-@app.route('/register',methods=['POST'])
+@app.route('/register',methods=['POST','GET'])
 def register():
     # Handle registration logic here
-    return render_template('player.html')
+    return render_template('register.html')
 
 @app.route('/game')
 def game():
@@ -30,6 +30,10 @@ def choice():
 
     return render_template('game.html', player=player_val2)
 
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    return render_template('login.html')
 
 if __name__=="__main__":
     app.run(debug=True)

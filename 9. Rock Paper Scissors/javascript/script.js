34051a802715b84e3f7b98ddd058954e13015626
Aryan Singh <singharyan90827@gmail.com>
2026-07-20 23:24:46 +0530

    winner checking feature added __tic-tac-toe

--- 9. Rock Paper Scissors/javascript/script.js
+++ 9. Rock Paper Scissors/javascript/script.js
@@ -5,24 +5,34 @@ const images1=document.querySelectorAll('img')[0];
 const images2=document.querySelectorAll('img')[1];
 const text=document.querySelectorAll('p');
 const choice=document.querySelector('.choices');
+const choicesids=document.querySelectorAll('.choice');
 
 let clicked=false;
+
+let computerScore=0;
+let humanScore=0;
+
 
 playBTN.addEventListener('click',() =>{
 
     if(clicked==false){
 
-            images1.style.opacity="0.02";
-            images2.style.opacity="0.02";
+            images1.style.opacity="0.1";
+            images2.style.opacity="0.1";
             body.classList.add('black-sheet');
             navbar.style.backgroundColor="#104911";
 
             text.forEach((txt) => {
-            txt.style.opacity = "0.02";
+            txt.style.opacity = "0.1";
             });
             choice.style.visibility="visible";
             playBTN.innerText="Leave";
             playBTN.style.backgroundColor="red";
+
+            choicesids.forEach((chc) => {
+            chc.classList.add('popup');
+            });
+
 
             clicked=true;
 
@@ -42,6 +52,10 @@ playBTN.addEventListener('click',() =>{
         choice.style.visibility="hidden";
         playBTN.innerText="Play";
         playBTN.style.backgroundColor="#104911";
+        choicesids.forEach((chc) => {
+            chc.classList.remove('popup');
+            });
+
         clicked=false;
     }
 
@@ -58,3 +72,42 @@ playBTN.addEventListener('click',() =>{
 //     });
 //     choice.style.visibility="hidden";
 // });
+
+function checkWinner(humanMove){
+    if(humanMove==ComputerMove){
+        console.log('Draw');
+    }
+    else if(humanMove=='rock')// rock vs paper and scissor
+        {
+        if(computerMove=='scissor'){
+            console.log('Human Winner');
+        }
+        else{
+            console.log('Computer Winner');
+        }
+    }
+    else if(humanMove=='paper'){ //paper vs scissor and rock
+        if(computerMove=='scissor'){
+            console.log('Computer Winner');
+        }
+        else{
+            console.log('Human Winner');
+        }
+    }
+    else //scissor vs rock and paper
+      {
+        if(computerMove=='rock'){
+            console.log('Computer Winner');
+        }
+        else{
+            console.log('Human Winner');
+        }
+    }
+}
+
+choicesids.forEach((choose) => {
+    choose.addEventListener('click',() =>{
+        let humanMove=choose.id;
+        checkWinner(humanMove);
+    })
+});

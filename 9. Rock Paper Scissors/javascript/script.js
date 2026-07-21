86daba5fe6e7bd5c0b45e894dd155a524aac08ae
Aryan Singh <singharyan90827@gmail.com>
2026-07-21 18:19:29 +0530

    messaging feature added in js __tic-tac-toe

--- 9. Rock Paper Scissors/javascript/script.js
+++ 9. Rock Paper Scissors/javascript/script.js
@@ -6,13 +6,61 @@ const images2=document.querySelectorAll('img')[1];
 const text=document.querySelectorAll('p');
 const choice=document.querySelector('.choices');
 const choicesids=document.querySelectorAll('.choice');
+const opt1=document.querySelector('.playerMove img');
+const opt2=document.querySelector('.computerMove img');
+const compScore=document.querySelector('#computerScore');
+const hScore=document.querySelector('#humanScore');
+const message=document.querySelector('.message');
 
 let clicked=false;
 
+
 let computerScore=0;
 let humanScore=0;
+let moves=['rock','scissor','paper'];
+let computerMove;
 
 
+const humanwin = () =>{
+    humanScore=humanScore+1;
+    hScore.innerText=`Score :${humanScore}`;
+    message.innerText='You Win';
+    message.style.backgroundColor='#104911';
+    message.visibility='visible';
+}
+
+const computerwin = () =>{
+    computerScore=computerScore+1;
+    compScore.innerText=`Score :${computerScore}`;
+    message.innerText='Computer Win';
+    message.style.backgroundColor='#9c4506';
+    message.visibility='visible';
+
+}
+
+
+
+const removeBlacksheet = () => {
+        images1.style.opacity="1";
+        images2.style.opacity="1";
+        body.classList.remove('black-sheet');
+        navbar.style.backgroundColor="#548C2F";
+
+        text.forEach((txt) => {
+        txt.style.opacity = "1";
+        });
+        choice.style.visibility="hidden";
+        playBTN.innerText="Play";
+        playBTN.style.backgroundColor="#104911";
+        choicesids.forEach((chc) => {
+            chc.classList.remove('popup');
+            });
+
+        clicked=false;
+}
+
+
+// console.log(Math.floor(Math.random()*3))
 playBTN.addEventListener('click',() =>{
 
     if(clicked==false){
@@ -41,22 +89,7 @@ playBTN.addEventListener('click',() =>{
     }
 
     else{
-        images1.style.opacity="1";
-        images2.style.opacity="1";
-        body.classList.remove('black-sheet');
-        navbar.style.backgroundColor="#548C2F";
-
-        text.forEach((txt) => {
-        txt.style.opacity = "1";
-        });
-        choice.style.visibility="hidden";
-        playBTN.innerText="Play";
-        playBTN.style.backgroundColor="#104911";
-        choicesids.forEach((chc) => {
-            chc.classList.remove('popup');
-            });
-
-        clicked=false;
+        removeBlacksheet();
     }
 
 });
@@ -73,41 +106,117 @@ playBTN.addEventListener('click',() =>{
 //     choice.style.visibility="hidden";
 // });
 
-function checkWinner(humanMove){
-    if(humanMove==ComputerMove){
-        console.log('Draw');
+
+const things = () => {
+    removeBlacksheet();
+
+
+}
+
+
+
+// function checkWinner(humanMove){
+//     if(humanMove==computerMove){
+//         if(humanMove=='rock'){
+//             opt1.src="images/Rock.jpg";
+//             opt2.src='images/Rock.jpg';
+//         }
+//         else if(humanMove=='scissor'){
+//             opt1.src="images/Scissor.jpg";
+//             opt2.src="images/Scissor.jpg";
+            
+//         }
+//         else{
+//             opt1.src="images/Paper.jpg";
+//             opt2.src="images/Paper.jpg";
+//         }
+//         console.log('Draw');
+//     }
+//     else if(humanMove=='rock')// rock vs paper and scissor
+            
+//         {
+//             opt1.src="images/Rock.jpg";
+//         if(computerMove=='scissor'){
+//             opt2.src='images/Scissor.jpg';
+//             console.log('Human Winner');
+//         }
+//         else{
+//             opt2.src="images/Paper.jpg";
+//             console.log('Computer Winner');
+//         }
+//     }
+//     else if(humanMove=='paper'){ 
+//         opt1.src="images/Paper.jpg";
+        
+//         //paper vs scissor and rock
+//         if(computerMove=='scissor'){
+//             opt2.src='images/Scissor.jpg';
+//             console.log('Computer Winner');
+//         }
+//         else{
+//             opt2.src="images/Rock.jpg";
+//             console.log('Human Winner');
+//         }
+//     }
+//     else //scissor vs rock and paper
+//       {
+//         opt1.src="images/Scissor.jpg";
+//         if(computerMove=='rock'){
+//             opt2.src="images/Rock.jpg";
+//             console.log('Computer Winner');
+//         }
+//         else{
+//             opt2.src="images/Paper.jpg";
+//             console.log('Human Winner');
+//         }
+//     }
+// }
+
+function checkWinner(humanMove) {
+
+    // Set the images first
+    opt1.src = `images/${humanMove.charAt(0).toUpperCase() + humanMove.slice(1)}.jpg`;
+    opt2.src = `images/${computerMove.charAt(0).toUpperCase() + computerMove.slice(1)}.jpg`;
+
+    if (humanMove == computerMove) {
+            message.innerText='Match Draw';
+            message.style.backgroundColor='black';
+            message.visibility='visible';
     }
-    else if(humanMove=='rock')// rock vs paper and scissor
-        {
-        if(computerMove=='scissor'){
-            console.log('Human Winner');
-        }
-        else{
-            console.log('Computer Winner');
-        }
-    }
-    else if(humanMove=='paper'){ //paper vs scissor and rock
-        if(computerMove=='scissor'){
-            console.log('Computer Winner');
-        }
-        else{
-            console.log('Human Winner');
+    else if (humanMove == "rock") {
+        if (computerMove == "scissor") {
+            console.log("Human Winner");
+            humanwin();
+        } else {
+            computerwin();
         }
     }
-    else //scissor vs rock and paper
-      {
-        if(computerMove=='rock'){
-            console.log('Computer Winner');
+    else if (humanMove == "paper") {
+        if (computerMove == "scissor") {
+            computerwin();
+        } else {
+            humanwin();
         }
-        else{
-            console.log('Human Winner');
+    }
+    else { // scissor
+        if (computerMove == "rock") {
+            computerwin();
+        } else {
+            humanwin();
         }
     }
 }
 
+
+
+
+
 choicesids.forEach((choose) => {
     choose.addEventListener('click',() =>{
         let humanMove=choose.id;
+        computerMove=moves[Math.floor(Math.random()*3)];
+        console.log(computerMove);
+        things();
         checkWinner(humanMove);
     })
 });

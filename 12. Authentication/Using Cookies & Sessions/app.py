ea39cda424625c5de9c513d130f3430226581343
Aryan Singh <singharyan90827@gmail.com>
2026-08-30 15:11:06 +0530

    Login functionality with session added

--- 12. Authentication/Using Cookies & Sessions/app.py
+++ 12. Authentication/Using Cookies & Sessions/app.py
@@ -1,17 +1,40 @@
-from fastapi import FastAPI , Request
+from fastapi import FastAPI , Request ,Form , Depends , HTTPException , Response 
 from fastapi.templating import Jinja2Templates
 from smtplib import SMTP
 from email.message import EmailMessage
 import os
+from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 import random
-
+from starlette.middleware.sessions import SessionMiddleware
+from typing import Annotated
+from db import SessionLocal , User
+from datetime import datetime , timezone
+import secrets
+from fastapi.responses import RedirectResponse
 
 load_dotenv()
 
 app=FastAPI()
 
 templates=Jinja2Templates(directory='templates')
+
+from starlette.middleware.sessions import SessionMiddleware
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=secrets.token_urlsafe(32),
+    max_age=60
+)
+
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 def send_otp():
     msg=EmailMessage()
@@ -39,4 +62,57 @@ def home(request : Request ):
 @app.get('/register')
 def register(request : Request ):
     return templates.TemplateResponse(request=request  , name="register.html")
+
+
+@app.get('/main')
+def main1(request: Request):
+
+    user_id = request.session.get("user_id")
+
+    if not user_id:
+        return RedirectResponse(url='/', status_code=303)
+
+    return templates.TemplateResponse(
+        request=request,
+        name='main.html'
+    )
+
+
+@app.post('/main')
+def main2(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name='main.html'
+    )
+
+
+@app.post('/login')
+async def login(
+    request: Request,
+    db: Annotated[Session, Depends(get_db)]):
+    form = await request.form()
+
+    email = form['email']
+    pswd = form['pswd']
+
+    verify = db.query(User).filter(User.email == email).first()
+
+    if not verify:
+        raise HTTPException(
+            status_code=401,
+            detail='Invalid Email'
+        )
+
+    if verify.password !=pswd:
+        raise HTTPException(
+            status_code=401,
+            detail='Invalid Password'
+        )
+
+    # Generate the ID
+    request.session['user_id']=verify.id
+    request.session['email']=verify.email
+    request.session['logged_at']=datetime.now(timezone.utc).isoformat()
+
+    return RedirectResponse(url='/main')
 

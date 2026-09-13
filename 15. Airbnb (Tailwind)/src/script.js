f1d0291b40c4df0637462fc339258acef262cd97
Aryan Singh <singharyan90827@gmail.com>
2026-09-13 23:41:18 +0530

    section-3 and carasoul added

--- 15. Airbnb (Tailwind)/src/script.js
+++ 15. Airbnb (Tailwind)/src/script.js
@@ -1,12 +1,1 @@
-let search = document.querySelector('.search-bar');
-
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= 800) {
-    search.classList.remove('hidden');
-  }else{
-    search.classList.add('hidden');
-  }
-  
-});
-
+const search = document.querySelector('.search-bar'); window.addEventListener('scroll', () => { if (window.scrollY >= 800) { search.classList.add('show'); } else { search.classList.remove('show'); } });

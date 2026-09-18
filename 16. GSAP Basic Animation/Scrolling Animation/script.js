3ae417e49659fcc087b5e02c73662bb64fbf79e4
Aryan Singh <singharyan90827@gmail.com>
2026-09-18 13:01:07 +0530

    Scrolling Animation

---
+++ 16. GSAP Basic Animation/Scrolling Animation/script.js
@@ -0,0 +1,33 @@
+
+
+
+window.addEventListener("wheel",(e)=> {
+    if(e.deltaY<0){
+    gsap.to(".marque",{
+    transform:"translateX(-200%)",
+    duration:2,
+    repeat:-1,
+    ease:'none'
+
+
+    }) 
+
+    gsap.to("i",{
+        rotate:180
+    })
+
+    }
+        else{
+        gsap.to(".marque",{
+        transform:"translateX(0%)",
+        duration:2,
+        repeat:-1,
+        ease:'none',
+    
+    })
+
+        gsap.to("i",{
+        rotate:0
+    })
+        }
+})
